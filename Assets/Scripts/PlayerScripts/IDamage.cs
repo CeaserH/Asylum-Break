e23ac7eee95527c46c6552cdc52619fd12f182cd
Ceaser Hernandez <ceaser.r.hernandez@gmail.com>
2025-05-14 19:50:00 -0700

    Added music to main menu

--- Assets/Scripts/PlayerScripts/IDamage.cs
+++ Assets/Scripts/PlayerScripts/IDamage.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IDamage
-{
-    void takeDamage(int amount);
-}
+//public interface IDamage
+//{
+//    void takeDamage(int amount);
+//}
